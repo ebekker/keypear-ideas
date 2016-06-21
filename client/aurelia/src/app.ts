b535ceb36a31a18053263c09e2ed560c40a867d7
Eugene Bekker <ebekker@powervision.com>
2016-06-21 13:09:45 -0400

    Cleaning up content and routing

--- client/aurelia/src/app.ts
+++ client/aurelia/src/app.ts
@@ -1,11 +1,15 @@
 import {Aurelia} from 'aurelia-framework';
 import {Router, RouterConfiguration, NavigationInstruction} from 'aurelia-router';
+import {AboutKeypear} from './about';
 import {KeypearMx} from './mx/mx';
 import {KeypearSx} from './sx/sx';
 import {FBTest} from './fb/fb';
 
+declare function require(string): string;
+
 export class App {
   router: Router;
+  logoUrl = require('./static/Keypear-v2-small.png');
 
   configureRouter(config: RouterConfiguration, router: Router) {
 
@@ -14,13 +18,14 @@ export class App {
     //    https://github.com/aurelia/router/issues/303
     //config.options.pushState = true;
 
-    config.title = 'Aurelia';
+    config.title = 'Keypear';
     config.map([
       { route: '', redirect: 'mx' },
       { route: 'mx',            name: 'mx',           moduleId: 'mx/mx',           nav: true,  title: 'MX'},
       { route: 'sx',            name: 'sx',           moduleId: 'sx/sx',           nav: true,  title: 'SX'},
-      { route: 'fb',            name: 'fb',           moduleId: 'fb/fb',           nav: true,  title: 'FB Test'},
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router',    nav: true,  title: 'Child Router' },
+    //{ route: 'fb',            name: 'fb',           moduleId: 'fb/fb',           nav: true,  title: 'FB Test'},
+    //{ route: 'child-router',  name: 'child-router', moduleId: 'child-router',    nav: true,  title: 'Child Router' },
+      { route: 'about',         name: 'about',        moduleId: 'about',           nav: true,  title: 'about' },
       { route: ':msgkey',       name: 'mx-msg',       moduleId: 'mx/mx-read',      nav: false, href: "mx" }
     ]);
     // Assume the default route is an MX message key,
