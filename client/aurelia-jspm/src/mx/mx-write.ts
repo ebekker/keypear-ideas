985ff8de702d7fd8d6d3d0dc5e9630e32bdeb45c
Eugene Bekker <ebekker@powervision.com>
2016-06-29 14:36:45 -0400

    Furthering final UX and crypto support

--- client/aurelia-jspm/src/mx/mx-write.ts
+++ client/aurelia-jspm/src/mx/mx-write.ts
@@ -1,21 +1,72 @@
 import { computedFrom } from 'aurelia-framework';
 
+import { KpCrypto, EphemeralMessage } from '../util/kp-crypto';
 import { ToastMaker } from '../util/toast';
 
 export class MxWriteCustomElement {
-  toastMaker = new ToastMaker();
+  message: string;
+  password: string;
+  tosAgree = false;
   readUrl: string;
+
+  toastMaker = new ToastMaker();
+
+  emStore: EphemeralMessage;
   
   computeReadUrl() {
     this.readUrl = `[${new Date().toString()}]`;
     this.toastMaker.addMessage(`Toasting @ ${this.readUrl}`);
   }
 
+  //@computedFrom('tosAgree', 'message')
+  get canSecureMessage() : boolean {
+    return this.tosAgree
+        && this.message != null
+        && this.message.trim().length > 0; 
+  }
+
   secureMessage() {
-    this.readUrl = 'foo';
+    if (!this.tosAgree) {
+      this.toastMaker.addWarning('You have to agree to the Terms of Service before you can continue');
+      return;
+    }
+    
+    let em = new EphemeralMessage();
+    em.message = this.message;
+    em.password = this.password;
+
+    this.message = null;
+    this.password = null;
+
+    KpCrypto.encryptMessage(em).then(() => {
+      this.readUrl = em.mekB64;
+      this.emStore = em;
+    });
   }
 
   clearMessage() {
-    this.readUrl = null;
+    let em = new EphemeralMessage()
+    em.saltB64 = this.emStore.saltB64;
+    em.ivB64 = this.emStore.ivB64;
+    em.messageEncB64 = this.emStore.messageEncB64;
+
+    em.mekB64 = this.readUrl;
+
+    KpCrypto.decryptMessage(em, () => {
+      this.message = em.message;
+      this.readUrl = null;
+    });
+  }
+
+  copyReadUrl() {
+    this.toastMaker.addMessage('URL copied to clipboard');
+  }
+
+  openReadUrl() {
+
+  }
+
+  sendReadUrl() {
+    alert('Email sending is not yet enabled');
   }
 }
